4149a78e7b9411458c2c6d858f9804806cb8524d
JohnnyQu4037 <xiaoqiqu@gmail.com>
2021-10-20 16:06:48 +1100

    unfinished homework
    the structure of the react components are slightly different than the ones learned from previous lesson, will come back to it when homework 5 is finished.

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,4 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
+import {useState,useRef} from "react"
+
+const editMode = ()=>{console.log(0)}
 
 const initialTasks = [
   {
@@ -14,70 +17,103 @@ const initialTasks = [
 ];
 
 const task = (
-  <div className='ui centered card'>
-    <div className='content'>
-      <div className='header'>
-        Clean up files
-      </div>
-      <div className='meta'>
-        Office chores
-      </div>
-      <div className='extra content'>
-        <span className='right floated edit icon'>
-          <i className='edit icon' />
-        </span>
-        <span className='right floated trash icon'>
-          <i className='trash icon' />
-        </span>
+  initialTasks.map((task)=>(
+    <div key = {task.id} className='ui centered card'>
+      <div className='content'>
+        <div className='header'>
+          {task.title}
+        </div>
+        <div className='meta'>
+          {task.project}
+        </div>
+        <div className='extra content'>
+          <span className='right floated edit icon'>
+            <i className='edit icon' onClick = {editMode}/>
+          </span>
+          <span className='right floated trash icon'>
+            <i className='trash icon' />
+          </span>
+        </div>
       </div>
     </div>
-  </div>
+  ))
 );
 
-const TaskForm = (
+const TaskForm =(addable,setAddable,tasks,setTasks)=>{
+  const titleRef = useRef()
+  const projectRef = useRef()
+
+  function taskHandler(event){
+    const newTask = {
+      title: 'Clean up files',
+      project: 'Office Chores',
+      id: uuidv4(),
+    }
+    tasks.push(newTask)
+    setTasks(tasks)
+  }
+
+  return (
   <div className='ui centered card'>
     <div className='content'>
       <div className='ui form'>
         <div className='field'>
           <label>Title</label>
-          <input type='text' />
+          <input type='text' ref = {titleRef}/>
         </div>
         <div className='field'>
           <label>Project</label>
-          <input type='text' />
+          <input type='text' ref={projectRef}/>
         </div>
         <div className='ui two bottom attached buttons'>
-          <button className='ui basic blue button'>
+          <button className='ui basic blue button' 
+            onClick = {()=>{
+              taskHandler()
+              addableHandler(addable,setAddable)
+            }}
+          >
             Create
           </button>
-          <button className='ui basic red button'>
+          <button className='ui basic red button' 
+            onClick = {()=>{
+              addableHandler(addable,setAddable)}}
+          >
             Cancel
           </button>
         </div>
       </div>
     </div>
   </div>
-);
+);} 
 
-const AddButton = (
-  <div className='ui basic content center aligned segment'>
-    <button className='ui basic button icon'>
-      <i className='plus icon' />
-    </button>
-  </div>
-);
+const AddButton = (addable,setAddable)=>{
+  return (
+    <div className='ui basic content center aligned segment'>
+      <button className='ui basic button icon' onClick = {()=>{addableHandler(addable,setAddable)}} >
+        <i className='plus icon' />
+      </button>
+    </div>
+  );
+}
 
 function App() {
+  const [addable,setAddable] = useState(true)
+  const [tasks,setTasks] = useState(initialTasks)
   return (
     <div className='ui three column centered grid'>
       <div className='column'>
         {task}
-        {TaskForm}
-        {AddButton}
+        {addable?null:TaskForm(addable,setAddable,tasks,setTasks)}
+        {addable?AddButton(addable,setAddable):null}
       </div>
     </div>
   );
 }
+
+function addableHandler(addable,setAddable){
+  setAddable(!addable)
+}
+
 
 export default App;
 
